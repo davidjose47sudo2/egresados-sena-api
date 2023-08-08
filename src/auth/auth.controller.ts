975e8b030a65e02bfaa9dd25847d4f9b55864f3b
davidjose47sudo2 <djsilva793@soy.sena.edu.co>
2023-08-08 11:50:56 -0500

    update same site lax

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -33,7 +33,7 @@ export class AuthController {
       httpOnly: false,
       maxAge: 1000 * 60 * 60 * 24,
       secure: true,
-      sameSite: 'none',
+      sameSite: 'lax',
       domain:"egresadosena-client.vercel.app",
     });
 
